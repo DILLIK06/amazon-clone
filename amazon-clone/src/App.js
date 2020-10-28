24ae20a3974d44741062c011accd33003e4dcacc
KD06 <48855537+KD06@users.noreply.github.com>
2020-10-28 12:00:50 +0530

    Initial commit

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello Dilli welcome to React, Have a great time learning it....! :rocket</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAg_aLv-JE5JFSQM2e4xHDND0NvP_dtKI",
+    authDomain: "clone-3328f.firebaseapp.com",
+    databaseURL: "https://clone-3328f.firebaseio.com",
+    projectId: "clone-3328f",
+    storageBucket: "clone-3328f.appspot.com",
+    messagingSenderId: "917232790255",
+    appId: "1:917232790255:web:05298052a49bfd3512491a",
+    measurementId: "G-THYPTEGN67"
+  };
